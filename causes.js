9c18ce78309af1a0decdec9d49ec04fd0c3094f4
Alex <agroleau@salesforce.com>
2018-04-25 19:40:20 -0400

    resolver updated

--- causes.js
+++ causes.js
@@ -1,10 +1,5 @@
 const imageUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/{cause}.png';
 const jsonUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/{cause}.json';
 
-const getImageUrl = (cause) => imageUrlTemplate.replace('{cause}', cause);
-const getJsonUrl = (cause) => jsonUrlTemplate.replace('{cause}', cause);
-
-export {
-  getImageUrl,
-  getJsonUrl
-};
+module.exports.getImageUrl = (cause) => imageUrlTemplate.replace('{cause}', cause);
+module.exports.getJsonUrl = (cause) => jsonUrlTemplate.replace('{cause}', cause);

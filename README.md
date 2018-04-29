deb98731049df02ec0e55e0f33d4d2ed2ceea7ff
Alex <agroleau@salesforce.com>
2018-04-28 21:48:59 -0400

    added logo and badger

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## General Dependencies
 - NodeJS v8.6.0+
+
+## Public to NPM
+- `npm run publish`
--- causes.js
+++ causes.js
@@ -1,5 +1,7 @@
-const imageUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/{cause}.png';
+const logoUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/logos/{image}.png';
+const badgeUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/badges/{image}.png';
 const jsonUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/{cause}.json';
 
-module.exports.getImageUrl = (cause) => imageUrlTemplate.replace('{cause}', cause);
+module.exports.getLogoUrl = (image) => logoUrlTemplate.replace('{image}', image);
+module.exports.getBadgeUrl = (image) => badgeUrlTemplate.replace('{image}', image);
 module.exports.getJsonUrl = (cause) => jsonUrlTemplate.replace('{cause}', cause);

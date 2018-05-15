14886b31fd838b9d0df531c8bd529af757f4416a
awgneo <agroleau@teampalmtree.com>
2018-05-14 20:52:51 -0400

    added background resolution

--- causes.js
+++ causes.js
@@ -1,7 +1,9 @@
 const logoUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/logos/{image}.png';
 const badgeUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/badges/{image}.png';
+const backgroundUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/backgrounds/{image}.png';
 const jsonUrlTemplate = 'https://raw.githubusercontent.com/seedom-io/seedom-assets/master/causes/{cause}.json';
 
 module.exports.getLogoUrl = (image) => logoUrlTemplate.replace('{image}', image);
 module.exports.getBadgeUrl = (image) => badgeUrlTemplate.replace('{image}', image);
+module.exports.getBackgroundUrl = (image) => backgroundUrlTemplate.replace('{image}', image);
 module.exports.getJsonUrl = (cause) => jsonUrlTemplate.replace('{cause}', cause);
